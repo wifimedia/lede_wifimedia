3d446b1a48fc050c4b2bc96d6740f369970c649d
DungTD <duntd8x@gmail.com>
2017-09-24 22:47:12 +0700

    advance

---
+++ advance/files/sbin/wifimedia/clear.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Copyright © 2017 Wifimedia.vn.
+# All rights reserved.
+
+gateway=$(route -n | grep 'UG' | grep 'br-wan' | awk '{ print $2 }')
+
+#check gateway
+ping -c 3 "$gateway" > /dev/null
+if [ $? -eq "0" ];then
+	cd /sys/devices/platform/leds-gpio/leds/tp-link:*:qss/
+	echo timer > trigger
+else
+	cd /sys/devices/platform/leds-gpio/leds/tp-link:*:qss/
+	echo none > trigger
+fi
+
+#checking internet
+ping -c 10 "8.8.8.8" > /dev/null
+if [ $? -eq "0" ];then
+	cd /sys/devices/platform/leds-gpio/leds/tp-link:*:wan/
+	echo timer > trigger
+else
+	cd /sys/devices/platform/leds-gpio/leds/tp-link:*:wan/
+	echo none > trigger
+fi
+#Clear memory
+if [ "$(cat /proc/meminfo | grep 'MemFree:' | awk '{print $2}')" -lt 5000 ]; then
+	sync && echo 3 > /proc/sys/vm/drop_caches
+fi
