5c9e69e15db33771c877f1b59e4d7271c82fb9a8
DungTD <duntd8x@gmail.com>
2017-09-26 14:50:49 +0700

    led for tplink_840nv4

--- advance/files/sbin/wifimedia/clear.sh
+++ advance/files/sbin/wifimedia/clear.sh
@@ -8,14 +8,14 @@ gateway=$(route -n | grep 'UG' | grep 'br-wan' | awk '{ print $2 }')
 ping -c 3 "$gateway" > /dev/null
 if [ $? -eq "0" ];then
 	#cd /sys/devices/platform/leds-gpio/leds/tp-link:*:qss/
-	cd /sys/devices/platform/gpio-leds/leds/tl-wr841n-v13:*:wps/
-	echo timer > trigger
+	#cd /sys/devices/platform/gpio-leds/leds/tl-wr841n-v13:*:wps/
+	#echo timer > trigger
 	cd /sys/devices/platform/gpio-leds/leds/tl-wr840n-v4:*:wps/
 	echo timer > trigger
 else
 	#cd /sys/devices/platform/leds-gpio/leds/tp-link:*:qss/
-	cd /sys/devices/platform/gpio-leds/leds/tl-wr841n-v13:*:wps/
-	echo none > trigger
+	#cd /sys/devices/platform/gpio-leds/leds/tl-wr841n-v13:*:wps/
+	#echo none > trigger
 	cd /sys/devices/platform/gpio-leds/leds/tl-wr840n-v4:*:wps/
 	echo none > trigger
 fi
@@ -23,17 +23,17 @@ fi
 #checking internet
 ping -c 10 "8.8.8.8" > /dev/null
 if [ $? -eq "0" ];then
-	cd /sys/devices/platform/gpio-leds/leds/tl-wr841n-v13:*:wan/
-	echo timer > trigger
+	#cd /sys/devices/platform/gpio-leds/leds/tl-wr841n-v13:*:wan/
+	#echo timer > trigger
 	#cd /sys/devices/platform/leds-gpio/leds/tp-link:*:wan/
 	cd /sys/devices/platform/gpio-leds/leds/tl-wr840n-v4:*:wan/
 	echo timer > trigger
 else
-	cd /sys/devices/platform/gpio-leds/leds/tl-wr841n-v13:*:wan/
+	#cd /sys/devices/platform/gpio-leds/leds/tl-wr841n-v13:*:wan/
 	#cd /sys/devices/platform/leds-gpio/leds/tp-link:*:wan/
-	echo none > trigger
+	#echo none > trigger
 	cd /sys/devices/platform/gpio-leds/leds/tl-wr840n-v4:*:wan/
-	echo timer > trigger	
+	echo timer > trigger
 fi
 #Clear memory
 if [ "$(cat /proc/meminfo | grep 'MemFree:' | awk '{print $2}')" -lt 5000 ]; then
