1b21331a7c699528142823608ddeb5abb973f01c
DungTD <duntd8x@gmail.com>
2017-11-01 08:37:39 +0700

    update core

--- advance/files/sbin/wifimedia/preauthen_rsn.sh
+++ advance/files/sbin/wifimedia/preauthen_rsn.sh
@@ -9,7 +9,7 @@ if [ ! -d "/tmp/upgrade" ]; then mkdir /tmp/upgrade; fi
 hardware=/tmp/upgrade/hardware
 echo "" > $hardware
 
-echo "Waiting a bit..."
+echo "Waiting a bit...802.11i"
 sleep $(head -30 /dev/urandom | tr -dc "0123456789" | head -c1)
 device=$(ifconfig br-lan | grep 'HWaddr' | awk '{ print $5 }'|sed 's/:/-/g')
 # Defines the URL to check the firmware at
@@ -21,17 +21,16 @@ curl_result=$?
 if [ "${curl_result}" -eq 0 ]; then
 	if grep -q "." $hardware; then
 		cat "$hardware" | while read line ; do
-			if [ "$(uci get wifimedia.@sync[0].rsn)" != "$(echo $line | awk '{print $1}')" ]; then
-				echo "Switch off hardware"
+			if [ "$(uci get wifimedia.@sync[0].rsn)" != "$(echo $line | awk '{print $6}')" ]; then
 				if [ "$(echo $line | grep $device)" ] ;then
 					#802.11i passwifi radio master
 					uci set wireless.@wifi-iface[0].ssid="PDA"
 					uci set wireless.@wifi-iface[0].encryption="mixed-psk"
 					uci set wireless.@wifi-iface[0].key="123456A@"
 					uci set wireless.@wifi-iface[0].rsn_preauth=1
-					uci set wireless.@wifi-iface[0]ieee80211r=0
+					uci set wireless.@wifi-iface[0].ieee80211r=0
 					uci commit wireless
-					uci get wifimedia.@sync[0].rsn="$(echo $line | awk '{print $1}')"
+					uci set wifimedia.@sync[0].rsn="$(echo $line | awk '{print $6}')"
 					uci commit wifimedia
 					wifi
 				else
@@ -39,8 +38,8 @@ if [ "${curl_result}" -eq 0 ]; then
 				fi
 			fi	
 		done	
-	else
-		echo "Could not connect to the upgrade server, exiting..."
+	#else
+	#	echo "Could not connect to the upgrade server, exiting..."
 	fi
 else
 	echo "Could not connect to the upgrade server, exiting..."
