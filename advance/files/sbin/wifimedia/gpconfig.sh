3d2cfa328995d04553a33749ce37d25553c0522f
DungTD <duntd8x@gmail.com>
2017-11-10 01:33:29 +0700

    fix

--- advance/files/sbin/wifimedia/gpconfig.sh
+++ advance/files/sbin/wifimedia/gpconfig.sh
@@ -38,13 +38,8 @@ if [ "${curl_result}" -eq 0 ]; then
 		
 			if [ "$(echo $line | grep $device)" ] ;then #tim thiet bi xem co trong groups hay khong
 	
-				uci delete wireless.@wifi-iface[0]
+				#uci delete wireless.@wifi-iface[0]
 				uci delete wireless.@wifi-iface[1]
-
-				if [ -z "$(uci get wireless.@wifi-iface[0])" ]; then 
-					uci add wireless wifi-iface; 
-				fi
-				uci set wireless.@wifi-iface[0].device="radio0"
 				uci set wireless.@wifi-iface[0].disabled="0"
 				uci commit wireless
 				
@@ -106,7 +101,7 @@ if [ "${curl_result}" -eq 0 ]; then
 							uci set wireless.@wifi-device[0].txpower=23						
 						fi
 					fi
-					
+					wifi up
 					####Auto reboot every day
 					if [ "$(echo $line | grep 'Reboot')" ] ;then #Auto Reboot every day
 						if [ "$(echo $line | awk '{print $2}')" == "1"  ];then
@@ -144,14 +139,18 @@ if [ "${curl_result}" -eq 0 ]; then
 						uci set scheduled.time.hour=0
 					fi
 					####END Auto reboot every day
+					uci commit wireless
+					uci commit scheduled
 				done
 				uci commit wireless
 				uci commit scheduled
-				sleep 5
-				wifi up
+
+				# Restart all of the services
+				/etc/init.d/network restart
 			fi	
 		done	
 	fi
 else
 	echo "Could not connect to the upgrade server, exiting..."
 fi
+
