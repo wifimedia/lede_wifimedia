0fc120cf49c07f28a4c1ad4faf04142b15b856c7
DungTD <duntd8x@gmail.com>
2017-11-17 05:53:20 +0700

    fix

--- advance/files/usr/lib/lua/luci/controller/wifimedia/advance.lua
+++ advance/files/usr/lib/lua/luci/controller/wifimedia/advance.lua
@@ -5,6 +5,6 @@ Copyright 2014 dungtd8x <dungtd8x@gmail.com>
 module("luci.controller.wifimedia.advance", package.seeall)
 function index()
 	entry( { "admin", "wifimedia"}, firstchild(), "Wifimedia", 50).dependent=false
-	entry( { "admin", "wifimedia", "advance" }, cbi("wifimedia_module/advance"), _("Controller"),      10)
+	entry( { "admin", "wifimedia", "controller" }, cbi("wifimedia_module/advance"), _("Controller"),      10)
 	entry( { "admin", "wifimedia", "info"    }, template("wifimedia_view/index"),    _("Info"), 80)
 end
