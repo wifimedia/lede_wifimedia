1b1b95d2986fac70427ac607643b078bf861a2c5
DungTD <duntd8x@gmail.com>
2017-11-22 16:54:23 +0700

    fix

--- advance/files/sbin/wifimedia/licensekey.sh
+++ advance/files/sbin/wifimedia/licensekey.sh
@@ -6,24 +6,25 @@
 if [ ! -d "/tmp/upgrade" ]; then mkdir /tmp/upgrade; fi
 
 # Wipes out previous upgrade information from dashboard
-hardware=/tmp/upgrade/hardware
-echo "" > $hardware
+licensekey=/tmp/upgrade/licensekey
+echo "" > $licensekey
 
 echo "Waiting a bit..check license key."
 sleep $(head -30 /dev/urandom | tr -dc "0123456789" | head -c1)
 device=$(ifconfig br-lan | grep 'HWaddr' | awk '{ print $5 }'|sed 's/:/-/g')
 # Defines the URL to check the firmware at
 url="http://firmware.wifimedia.com.vn/hardware_active"
-wget -q "${url}" -O $hardware
+wget -q "${url}" -O $licensekey
 curl_result=$?
 
 if [ "${curl_result}" -eq 0 ]; then
-	if grep -q "." $hardware; then
-		cat "$hardware" | while read line ; do
+	if grep -q "." $licensekey; then
+		cat "$licensekey" | while read line ; do
 			if [ "$(echo $line | grep $device)" ] ;then
 				#Update License Key
 				uci -q get wifimedia.@advance[0].wfm="$(cat /etc/opt/license/wifimedia)"
 				uci commit wifimedia
+				/usr/bin/license.sh
 			else
 				echo "we will maintain the existing settings."
 			fi
