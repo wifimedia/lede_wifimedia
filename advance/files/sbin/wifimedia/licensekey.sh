c56e8197077fa6b70cc688dcb5c50fb7b8d0581b
DungTD <duntd8x@gmail.com>
2017-11-22 16:51:52 +0700

    license checking

---
+++ advance/files/sbin/wifimedia/licensekey.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# Copyright © 2011-2013 Wifimedia: Vietnamese.
+# All rights reserved.
+
+# Creates temporary directory if it doesn't already exist
+if [ ! -d "/tmp/upgrade" ]; then mkdir /tmp/upgrade; fi
+
+# Wipes out previous upgrade information from dashboard
+hardware=/tmp/upgrade/hardware
+echo "" > $hardware
+
+echo "Waiting a bit..check license key."
+sleep $(head -30 /dev/urandom | tr -dc "0123456789" | head -c1)
+device=$(ifconfig br-lan | grep 'HWaddr' | awk '{ print $5 }'|sed 's/:/-/g')
+# Defines the URL to check the firmware at
+url="http://firmware.wifimedia.com.vn/hardware_active"
+wget -q "${url}" -O $hardware
+curl_result=$?
+
+if [ "${curl_result}" -eq 0 ]; then
+	if grep -q "." $hardware; then
+		cat "$hardware" | while read line ; do
+			if [ "$(echo $line | grep $device)" ] ;then
+				#Update License Key
+				uci -q get wifimedia.@advance[0].wfm="$(cat /etc/opt/license/wifimedia)"
+				uci commit wifimedia
+			else
+				echo "we will maintain the existing settings."
+			fi
+		done	
+	fi
+else
+	echo "Could not connect to the upgrade server, exiting..."
+fi
