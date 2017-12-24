382bcade8a7c7441689e47ef236c4b12de09ab17
DungTD <duntd8x@gmail.com>
2017-12-24 13:59:48 +0700

    fix mode bridge

--- advance/files/usr/lib/lua/luci/model/cbi/wifimedia_module/advance.lua
+++ advance/files/usr/lib/lua/luci/model/cbi/wifimedia_module/advance.lua
@@ -7,11 +7,13 @@ local sys = require "luci.sys"
 local fs = require "nixio.fs"
 local uci = require "luci.model.uci".cursor()
 local wfm_lcs = fs.access("/etc/opt/wfm_lcs")
-local online = fs.access("/etc/opt/online")
+--local online = fs.access("/etc/opt/online")
 m = Map("wifimedia", "")
 function m.on_after_commit(self)
 	luci.sys.call("env -i /usr/bin/license.sh start >/dev/null")
-	luci.sys.call("env -i /sbin/wifimedia/groups.sh start >/dev/null")
+	--luci.sys.call("env -i /sbin/wifimedia/groups.sh start >/dev/null")
+	luci.sys.call("env -i /bin/ubus call network reload >/dev/null 2>/dev/null")
+	--luci.http.redirect(luci.dispatcher.build_url("admin","wifimedia","advance"))
 end
 
 s = m:section(TypedSection, "advance","")
@@ -20,7 +22,7 @@ s.addremove = false
 
 s:tab("rssi",  translate("RSSI"))
 	--s:taboption("rssi", Value, "pinginterval","Interval (s)").placeholder = "interval"
-	rssi = s:taboption("rssi", Flag, "enable","Status")
+	rssi = s:taboption("rssi", Flag, "enable","Enable")
 	rssi.rmempty = false
 		function rssi.write(self, section, value)
 			if value == self.enabled then
@@ -47,6 +49,40 @@ if online then
 	rm.rmempty = false
 end
 ]]--
+s:tab("bridge_network",  translate("Bridge Network"))
+bridge_mode = s:taboption("bridge_network", Flag, "bridge_mode","Bridge","Ethernet:  wan => lan")
+bridge_mode.rmempty = false
+		function bridge_mode.write(self, section, value)
+			if value == self.enabled then
+				luci.sys.call("uci delete network.lan")
+				--luci.sys.call("uci set network.lan='interface'")
+				--luci.sys.call("uci set network.lan.proto='dhcp'")
+				--luci.sys.call("uci set network.lan.ifname='eth1'")
+				--luci.sys.call("uci set network.lan.ipaddr='172.16.99.1'")
+				--luci.sys.call("uci set network.lan.netmask='255.255.255.0'")
+				--luci.sys.call("uci delete network.lan.type='bridge'")
+				luci.sys.call("uci set network.wan.proto='dhcp'")
+				luci.sys.call("uci set network.wan.ifname='eth0 eth1'")
+				luci.sys.call("uci set wireless.@wifi-iface[0].network='wan'")
+
+				luci.sys.call("uci commit")
+			else
+				luci.sys.call("uci set network.lan.proto='static'")
+				luci.sys.call("uci set network.lan.ipaddr='172.16.99.1'")
+				luci.sys.call("uci set network.lan.netmask='255.255.255.0'")
+				luci.sys.call("uci set network.lan.type='bridge'")
+				luci.sys.call("uci set dhcp.lan.force='1'")
+				luci.sys.call("uci set dhcp.lan.netmask='255.255.255.0'")
+				luci.sys.call("uci del dhcp.lan.dhcp_option")
+				luci.sys.call("uci add_list dhcp.lan.dhcp_option='6,8.8.8.8,8.8.4.4'")				
+				luci.sys.call("uci set network.wan.ifname='eth0'")
+				luci.sys.call("uci set wireless.@wifi-iface[0].network='wan'")
+				luci.sys.call("uci commit")		
+			end
+			return Flag.write(self, section, value)
+		end
+		function bridge_mode.remove() end
+
 if wfm_lcs then
 	s:tab("license",  translate("License"))
 	wfm = s:taboption("license",Value,"wfm","Key")
