b13e6a860833cfa570f4a6680d42926a0cc255c4
DungTD <duntd8x@gmail.com>
2017-12-26 17:34:45 +0700

    fix ap manager

--- advance/files/sbin/wifimedia/apmanager.sh
+++ advance/files/sbin/wifimedia/apmanager.sh
@@ -11,8 +11,8 @@ touch -c /etc/macaddress
 #EXPORT DATA AP MAC
 cat "$devices" | while read line ; do
 
-	mac=$(echo $line | awk '{print $2}' | sed 's/-/:/g' | tr a-z A-Z  | cut -d ':' -f1-5)
-	maclast=$(echo $line | awk '{print $2}' | sed 's/-/:/g' | tr a-z A-Z  | cut -d ':' -f6)
+	mac=$(echo $line | awk '{print $2}' | sed 's/-/:/g' | cut -d ':' -f1-5)
+	maclast=$(echo $line | awk '{print $2}' | sed 's/-/:/g' | cut -d ':' -f6)
 	decmac=$(echo "ibase=16; $maclast"|bc)
 	if [ $decmac -eq '241' ];then
 		macinc='00'
@@ -25,16 +25,6 @@ done
 
 #EXPORT DATA AP IP MAC
 cat "/etc/macaddress" | while read line ; do
-
-	linedev=$(echo $line | awk '{print $1}' | sed 's/-/:/g' | tr a-z A-Z)
-		
-		cat "$dhcp" | while read line ; do
-		
-			linedhcp=$(echo $line | awk '{print $2}' | sed 's/-/:/g' | tr a-z A-Z)
-			#echo $linedev
-			#echo $linedhcp
-			if [ "$linedev" == "$linedhcp" ] ;then
-				echo $line | awk '{print $2 " http://" $3 " " $3}' >>/etc/ap
-			fi
-		done
+	linedeap=$(echo $line | awk '{print $1}' | sed 's/-/:/g')
+	arp | grep $linedeap | awk '{print $4 " "$1 " http://" $1 }' >>/etc/ap
 done
