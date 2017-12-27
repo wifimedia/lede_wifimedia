2130177c888b7c33b141353410b920470e91abe8
DungTD <duntd8x@gmail.com>
2017-12-27 12:22:04 +0700

    fix buid date anhd advance

--- advance/files/usr/lib/lua/luci/model/cbi/wifimedia_module/advance.lua
+++ advance/files/usr/lib/lua/luci/model/cbi/wifimedia_module/advance.lua
@@ -20,35 +20,71 @@ s = m:section(TypedSection, "advance","")
 s.anonymous = true
 s.addremove = false
 
-s:tab("rssi",  translate("RSSI"))
-	--s:taboption("rssi", Value, "pinginterval","Interval (s)").placeholder = "interval"
-	rssi = s:taboption("rssi", Flag, "enable","Enable")
-	rssi.rmempty = false
-		function rssi.write(self, section, value)
-			if value == self.enabled then
-				luci.sys.call("env -i /etc/init.d/watchping start >/dev/null")
-				luci.sys.call("env -i /etc/init.d/watchping enable >/dev/null")
-			else
-				luci.sys.call("env -i /etc/init.d/watchping stop >/dev/null")
-				luci.sys.call("env -i /etc/init.d/watchping disable >/dev/null")
-			end
-			return Flag.write(self, section, value)
-		end
-		function rssi.remove() end
-	--else 
-	--	m.pageaction = false
+--[[ auto controller ]]--
+s:tab("ctrgroups",  translate("Wireless"))
+ctrgs_en = s:taboption("ctrgroups",Flag, "ctrs_en", "Enable")
+ctrgs = s:taboption("ctrgroups",Value, "essid", "SSID")
+ctrgs:depends({ctrs_en="1"})
 
-	t = s:taboption("rssi", Value, "level","RSSI:","Range:-60dBm ~ -90dBm")
-	t.datatype = "min(-90)"
-	--s:taboption("rssi",Value, "delays","Time Delays (s)").optional = false
-	--t:depends({enable="1"})
+ctrgsm = s:taboption("ctrgroups",ListValue, "mode", "MODE")
+ctrgsm:value("ap","AP")
+ctrgsm:value("mesh","MESH")
+ctrgsm:value("wds","WDS")
+ctrgsm:depends({ctrs_en="1"})
+
+ctrgscnl = s:taboption("ctrgroups",Value, "maxassoc", "Connection Limit")
+ctrgscnl:depends({ctrs_en="1"})
+
+ctrgsn = s:taboption("ctrgroups",ListValue, "network", "Network")
+ctrgsn:value("wan","WAN")
+ctrgsn:value("lan","LAN")
+ctrgsn:depends({ctrs_en="1"})
+
+ctrgsn = s:taboption("ctrgroups",ListValue, "encrypt", "Wireless Security")
+ctrgsn:value("","No Encryption")
+ctrgsn:value("encryption","WPA-PSK/WPA2-PSK")
+ctrgsn:depends({ctrs_en="1"})
+
+grwpa = s:taboption("ctrgroups",Value, "password", "Password")
+grwpa.datatype = "wpakey"
+grwpa.rmempty = true
+grwpa.password = true
+grwpa:depends({encrypt="encryption"})
+
+ctrgsft = s:taboption("ctrgroups",ListValue, "ft", "Fast Roaming")
+ctrgsft:value("rsn_preauth","Fast-Secure Roaming")
+ctrgsft:value("ieee80211r","Fast Basic Service Set Transition (FT)")
+ctrgsft:depends({encrypt="encryption"})
+
+nasid = s:taboption("ctrgroups",Value, "nasid", "NAS ID")
+nasid:depends({ft="ieee80211r"})
+
+--macs.datatype = "macaddr"
+--[[Tx Power]]--
+ctrgtx = s:taboption("ctrgroups",ListValue, "txpower", "Transmit Power")
+ctrgtx:value("auto","Auto")
+ctrgtx:value("low","Low")
+ctrgtx:value("medium","Medium")
+ctrgtx:value("high","High")
+ctrgtx:depends({ctrs_en="1"})
+
+hidessid = s:taboption("ctrgroups",Flag, "hidessid","Hide SSID")
+hidessid.rmempty = false
+hidessid:depends({ctrs_en="1"})
+ 
+apisolation = s:taboption("ctrgroups",Flag, "isolation","AP Isolation")
+apisolation.rmempty = false
+apisolation:depends({ctrs_en="1"})
+--[[AP Groups]]--
 --[[
-if online then
-	s:tab("online",  translate("Staus Online"))
-	rm= s:taboption("online",Flag, "online", "Status","On/Off")
-	rm.rmempty = false
-end
+s:tab("device",  translate("AP Groups"))
+device = s:taboption("device",Flag, "gpd_en","Enable Groups")
+device.rmempty = false
+device = s:taboption("device",Value, "macs", "Devices")
+device:depends({gpd_en="1"})
+--[[END]]--
 ]]--
+
 s:tab("bridge_network",  translate("Bridge Network"))
 bridge_mode = s:taboption("bridge_network", Flag, "bridge_mode","Bridge","Ethernet:  wan => lan")
 bridge_mode.rmempty = false
@@ -83,92 +119,37 @@ bridge_mode.rmempty = false
 			return Flag.write(self, section, value)
 		end
 		function bridge_mode.remove() end
+		
+		
+--RSSI--
+s:tab("rssi",  translate("RSSI"))
+	--s:taboption("rssi", Value, "pinginterval","Interval (s)").placeholder = "interval"
+	rssi = s:taboption("rssi", Flag, "enable","Enable")
+	rssi.rmempty = false
+		function rssi.write(self, section, value)
+			if value == self.enabled then
+				luci.sys.call("env -i /etc/init.d/watchping start >/dev/null")
+				luci.sys.call("env -i /etc/init.d/watchping enable >/dev/null")
+			else
+				luci.sys.call("env -i /etc/init.d/watchping stop >/dev/null")
+				luci.sys.call("env -i /etc/init.d/watchping disable >/dev/null")
+			end
+			return Flag.write(self, section, value)
+		end
+		function rssi.remove() end
+	--else 
+	--	m.pageaction = false
 
+	t = s:taboption("rssi", Value, "level","RSSI:","Range:-60dBm ~ -90dBm")
+	t.datatype = "min(-90)"
+	--s:taboption("rssi",Value, "delays","Time Delays (s)").optional = false
+	--t:depends({enable="1"})
+--[[END RSSI]]--		
+--License
 if wfm_lcs then
 	s:tab("license",  translate("License"))
 	wfm = s:taboption("license",Value,"wfm","Key")
 	wfm.rmempty = true
 end
-s:tab("fastroaming",  translate("Fast Roaming"))
-ft = s:taboption("fastroaming",Flag, "ft_wifi", "Enable Fast Roaming")
-ft = s:taboption("fastroaming",Value, "essid", "SSIDs")
-ft:depends({ft_wifi="1"})
-
---[[
-s:tab("ctrgroups",  translate("Wireless Groups"))
-ctrgs_en = s:taboption("ctrgroups",Flag, "ctrs_en", "Enable Groups")
-ctrgs = s:taboption("ctrgroups",Value, "essid", "SSIDs")
-ctrgs:depends({ctrs_en="1"})
-
-ctrgsm = s:taboption("ctrgroups",ListValue, "mode", "MODE")
-ctrgsm:value("ap","AP")
-ctrgsm:value("mesh","MESH")
-ctrgsm:value("wds","WDS")
-ctrgsm:depends({ctrs_en="1"})
-
-ctrgscnl = s:taboption("ctrgroups",Value, "maxassoc", "Connection Limit")
-ctrgscnl:depends({ctrs_en="1"})
-
-ctrgsn = s:taboption("ctrgroups",ListValue, "network", "Network")
-ctrgsn:value("wan","WAN")
-ctrgsn:value("lan","LAN")
-ctrgsn:depends({ctrs_en="1"})
-
-grwpa = s:taboption("ctrgroups",Value, "password", "Password")
-grwpa.datatype = "wpakey"
-grwpa:depends({ctrs_en="1"})
-
-ctrgsft = s:taboption("ctrgroups",ListValue, "ft", "Fast Roaming")
-ctrgsft:value("rsn_preauth","RSN preauthentication")
-ctrgsft:value("ieee80211r","Fast Basic Service Set Transition (FT)")
-ctrgsft:depends({ctrs_en="1"})
-
-nasid = s:taboption("ctrgroups",Value, "nasid", "NAS ID")
-nasid:depends({ft="ieee80211r"})
-
---macs.datatype = "macaddr"
-
-ctrgtx = s:taboption("ctrgroups",ListValue, "txpower", "Transmit Power")
-ctrgtx:value("auto","Auto")
-ctrgtx:value("low","Low")
-ctrgtx:value("medium","Medium")
-ctrgtx:value("high","High")
-ctrgtx:depends({ctrs_en="1"})
-
-apisolation = s:taboption("ctrgroups",Flag, "isolation","AP Isolation")
-apisolation.rmempty = false
-apisolation:depends({ctrs_en="1"})
-
-s:tab("device",  translate("AP Groups"))
-device = s:taboption("device",Flag, "gpd_en","Enable Groups")
-device.rmempty = false
-device = s:taboption("device",Value, "macs", "Devices")
-device:depends({gpd_en="1"})
-
-
-s:tab("autoreboot",  translate("Reboot Groups"))
-Everyday = s:taboption("autoreboot",Flag, "Everyday","Everyday Auto Reboot")
-Everyday.rmempty = false
-
-h = s:taboption("autoreboot", ListValue, "hour", "Hours")
-local time = 0
-while (time < 24) do
-	h:value(time, time .. " ")
-	time = time + 1
-end
-h:depends({Everyday="1"})
-mi = s:taboption("autoreboot", ListValue, "minute", "Minutes")
-local minute = 0
-while (minute < 60) do
-	mi:value(minute, minute .. " ")
-	minute = minute + 1
-end
-mi:depends({Everyday="1"})
-
-s:tab("administrator",  translate("Administrators"))
-admingr = s:taboption("administrator",Flag, "admins", "Enable Groups")
-admingr = s:taboption("administrator",Value, "passwords", "Password")
-admingr:depends({admins="1"})
-]]--
-
+--[[END LICENS]]--
 return m
