15bf46f386cf0ccf1ce311f4fbae299c60f5415b
DungTD <duntd8x@gmail.com>
2017-12-27 22:59:53 +0700

    APID

--- advance/files/usr/lib/lua/luci/model/cbi/wifimedia_module/advance.lua
+++ advance/files/usr/lib/lua/luci/model/cbi/wifimedia_module/advance.lua
@@ -58,7 +58,7 @@ ctrgsft:depends({encrypt="encryption"})
 
 nasid = s:taboption("ctrgroups",Value, "nasid", "NAS ID")
 nasid:depends({ft="ieee80211r"})
-device = s:taboption("ctrgroups",Value, "macs", "AP ID")
+device = s:taboption("ctrgroups",Value, "macs", "APID")
 device:depends({ft="ieee80211r"})
 --macs.datatype = "macaddr"
 --[[Tx Power]]--
