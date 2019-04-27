8287cc1c470838c1e1d2537c597eb9626dea6d4c
wifimedia <dungtd8x@gmail.com>
2019-04-27 15:44:06 +0700

    ...

--- wifimedia_portal/files/sbin/wifimedia/update_preauthenticated_rules.sh
+++ wifimedia_portal/files/sbin/wifimedia/update_preauthenticated_rules.sh
@@ -11,15 +11,31 @@ while true; do
 done
 
 #sh /root/create_ssh_tunnel.sh
-
+#Value
 NODOGSPLASH_CONFIG=/tmp/etc/nodogsplash.conf
 PREAUTHENTICATED_ADDRS=/tmp/preauthenticated_addrs
 PREAUTHENTICATED_RULES=/tmp/preauthenticated_rules
 walledgadent=`uci get wifimedia.@nodogsplash[0].preauthenticated_users | sed 's/,/ /g'`
+domain=`uci -q get wifimedia.@nodogsplash[0].domain`
+domain_default=${domain:-portal.nextify.vn}
+redirecturl=`uci -q get wifimedia.@nodogsplash[0].redirecturl`
+redirecturl_default=${redirecturl:-https://google.com.vn}
+preauthenticated_users=`uci -q get wifimedia.@nodogsplash[0].preauthenticated_users` #Walled Gardent
+maxclients=`uci -q get wifimedia.@nodogsplash[0].maxclients`
+maxclients_default=${maxclients:-120}
+preauthidletimeout=`uci -q get wifimedia.@nodogsplash[0].preauthidletimeout`
+preauthidletimeout_default=${preauthidletimeout:-30}
+authidletimeout=`uci -q get wifimedia.@nodogsplash[0].authidletimeout`
+authidletimeout_default=${authidletimeout:-120}
+sessiontimeout=`uci -q get wifimedia.@nodogsplash[0].sessiontimeout`
+sessiontimeout_default=${sessiontimeout:-20}
+checkinterval=`uci -q get wifimedia.@nodogsplash[0].checkinterval`
+checkinterval_default=${checkinterval:-5}
+MAC_E0=$(ifconfig eth1 | grep 'HWaddr' | awk '{ print $5 }')
 
 # Whitelist IP
-for domain in portal.nextify.vn static.nextify.vn nextify.vn crm.nextify.vn $walledgadent; do
-    nslookup ${domain} 8.8.8.8 2> /dev/null | \
+for i in portal.nextify.vn static.nextify.vn nextify.vn crm.nextify.vn $walledgadent; do
+    nslookup ${i} 8.8.8.8 2> /dev/null | \
         grep 'Address ' | \
         grep -v '127\.0\.0\.1' | \
         grep -v '8\.8\.8\.8' | \
@@ -28,13 +44,71 @@ for domain in portal.nextify.vn static.nextify.vn nextify.vn crm.nextify.vn $wal
         grep -v ':' >> ${PREAUTHENTICATED_ADDRS}
 done
 ###Read line file 
-
+uci del nodogsplash.@nodogsplash[0].preauthenticated_users && uci commit
 uci add_list nodogsplash.@nodogsplash[0].preauthenticated_users="allow tcp port 53"
 uci add_list nodogsplash.@nodogsplash[0].preauthenticated_users="allow udp port 53"
 while read line; do
 	uci add_list nodogsplash.@nodogsplash[0].preauthenticated_users="allow to $(echo $line)"
 done <$PREAUTHENTICATED_ADDRS
+
+if [ "$redirecturl" != "" ];then
+	uci set nodogsplash.@nodogsplash[0].redirecturl=$redirecturl_default	
+elif [ "$maxclients" != ""];then
+	uci set nodogsplash.@nodogsplash[0].maxclients=$maxclients_default
+elif [ "$preauthidletimeout" != "" ];then
+	uci set nodogsplash.@nodogsplash[0].preauthidletimeout=$preauthidletimeout
+elif [ "$authidletimeout" != "" ];then
+	uci set nodogsplash.@nodogsplash[0].authidletimeout=$authidletimeout
+elif [ "$sessiontimeout" != "" ];then
+	sessiontimeout_=$(expr $sessiontimeout_default \* 60)
+	uci set nodogsplash.@nodogsplash[0].sessiontimeout=$sessiontimeout_	
+elif [ "$checkinterval" != "" ];then
+	checkinterval_=$(expr $checkinterval_default \* 60)
+	uci set nodogsplash.@nodogsplash[0].checkinterval=$checkinterval_
+fi
 uci commit
+rm -f $PREAUTHENTICATED_ADDRS
+#write file splash
+echo '<!doctype html>
+<html lang="en">
+  <head>
+      <meta charset="utf-8">
+      <title>$gatewayname</title>
+  </head>
+  <body>
+      <form id="info" method="POST" action="//'$domain'/splash">
+          <input type="hidden" name="gateway_name" value="$gatewayname">
+          <input type="hidden" name="gateway_mac" value="'$MAC_E0'">
+          <input type="hidden" name="client_mac" value="$clientmac">
+          <input type="hidden" name="num_clients" value="$nclients">
+          <input type="hidden" name="uptime" value="$uptime">
+          <input type="hidden" name="auth_target" value="$authtarget">
+      </form>
+      <script>
+          document.getElementById("info").submit();
+      </script>
+  </body>
+</html>' >/etc/nodogsplash/htdocs/splash.html
+
+#write file infoskel
+echo '<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Whoops...</title>
+        <meta http-equiv="refresh" content="0; url="//'$domain'/splash">
+        <style>
+            html {
+                background: #F7F7F7;
+            }
+        </style>
+    </head>
+    <body></body>
+</html>' >/etc/nodogsplash/htdocs/status.html
+#config option nodogsplash
+
+/etc/init.d/firewall restart
+
 
 #grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
 
