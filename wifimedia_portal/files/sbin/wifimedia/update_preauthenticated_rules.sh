aaf7fd36559f1ad4ca01e7f099b3c8b52e8694d9
wifimedia <dungtd8x@gmail.com>
2019-04-27 18:52:30 +0700

    Update update_preauthenticated_rules.sh

--- wifimedia_portal/files/sbin/wifimedia/update_preauthenticated_rules.sh
+++ wifimedia_portal/files/sbin/wifimedia/update_preauthenticated_rules.sh
@@ -15,7 +15,8 @@ done
 NODOGSPLASH_CONFIG=/tmp/etc/nodogsplash.conf
 PREAUTHENTICATED_ADDRS=/tmp/preauthenticated_addrs
 PREAUTHENTICATED_RULES=/tmp/preauthenticated_rules
-
+NET_ID="nextify"
+FW_ZONE="nextify"
 walledgadent=`uci get wifimedia.@nodogsplash[0].preauthenticated_users | sed 's/,/ /g'`
 domain=`uci -q get wifimedia.@nodogsplash[0].domain`
 domain_default=${domain:-portal.nextify.vn}
@@ -36,6 +37,8 @@ checkinterval_default=${checkinterval:-10}
 ctv=`expr $checkinterval_default \* 60`
 MAC_E0=$(ifconfig eth1 | grep 'HWaddr' | awk '{ print $5 }')
 
+nds (){
+uci set nodogsplash.@nodogsplash[0].gatewayinterface="br-${NET_ID}";
 uci set nodogsplash.@nodogsplash[0].redirecturl="$redirecturl_default";
 uci set nodogsplash.@nodogsplash[0].maxclients="$maxclients_default";
 uci set nodogsplash.@nodogsplash[0].preauthidletimeout="$preauthidletimeout_default";
@@ -101,9 +104,7 @@ echo '<!doctype html>
     <body></body>
 </html>' >/etc/nodogsplash/htdocs/status.html
 #config option nodogsplash
-
-/etc/init.d/firewall restart
-
+}
 
 #grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
 
@@ -136,3 +137,56 @@ echo '<!doctype html>
 #   	#echo timer > trigger
 ##	echo "Nodogsplash running"
 ##fi
+
+
+#Create Network
+nds_del() {
+uci batch << EOF
+	del network.${NET_ID}
+	del dhcp.${NET_ID}
+	del firewall.${FW_ZONE}
+EOF
+}
+nds_add_network(){
+	uci batch << EOF
+	set network.${NET_ID}=interface
+	set network.${NET_ID}.ifname=${NET_ID}
+	set network.${NET_ID}.proto=static
+	set network.${NET_ID}.type=bridge
+	set network.${NET_ID}.ipaddr=10.68.255.1
+	set network.${NET_ID}.netmask=255.255.255.0
+	set dhcp.${NET_ID}=dhcp
+	set dhcp.${NET_ID}.interface=${NET_ID}
+	set dhcp.${NET_ID}.start=100
+	set dhcp.${NET_ID}.leasetime=1h
+	set dhcp.${NET_ID}.limit=150
+	set firewall.${FW_ZONE}=zone
+	set firewall.${FW_ZONE}.name=${FW_ZONE}
+	set firewall.${FW_ZONE}.network=${NET_ID}
+	set firewall.${FW_ZONE}.forward=REJECT
+	set firewall.${FW_ZONE}.output=ACCEPT
+	set firewall.${FW_ZONE}.input=REJECT 
+	set firewall.${FW_ZONE}_fwd=forwarding
+	set firewall.${FW_ZONE}_fwd.src=${FW_ZONE}
+	set firewall.${FW_ZONE}_fwd.dest=wan
+	set firewall.${FW_ZONE}_dhcp=rule
+	set firewall.${FW_ZONE}_dhcp.name=${FW_ZONE}_DHCP
+	set firewall.${FW_ZONE}_dhcp.src=${FW_ZONE}
+	set firewall.${FW_ZONE}_dhcp.target=ACCEPT
+	set firewall.${FW_ZONE}_dhcp.proto=udp
+	set firewall.${FW_ZONE}_dhcp.dest_port=67-68
+	set firewall.${FW_ZONE}_dns=rule
+	set firewall.${FW_ZONE}_dns.name=${FW_ZONE}_DNS
+	set firewall.${FW_ZONE}_dns.src=${FW_ZONE}
+	set firewall.${FW_ZONE}_dns.target=ACCEPT
+	set firewall.${FW_ZONE}_dns.proto=tcpudp
+	set firewall.${FW_ZONE}_dns.dest_port=53
+EOF
+uci commit network
+uci commit dhcp
+uci commit firewall
+service network reload
+service dnsmasq restart
+service firewall restart
+}
+
