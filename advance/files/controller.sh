3c9ee1c1e84893b0875d50d899c11a5ffd4b2682
wifimedia <dungtd8x@gmail.com>
2019-05-08 18:30:57 +0700

    ....

--- advance/files/controller.sh
+++ advance/files/controller.sh
@@ -581,7 +581,7 @@ curl_result=$?
 if [ "${curl_result}" -eq 0 ]; then
 	if grep -q "." $licensekey; then
 		cat "$licensekey" | while read line ; do
-			if [ "$(echo $line | grep $apid)" ] ;then
+			if [ "$(echo $line | grep $cf_apid)" ] ;then
 				#Update License Key
 				uci set wifimedia.@advance[0].wfm="$(cat /etc/opt/license/wifimedia)"
 				uci commit wifimedia
@@ -603,7 +603,7 @@ lgw_srv() {
 	if [ "${curl_result}" -eq 0 ]; then
 		if grep -q "." $gwkey; then
 			cat "$licensekey" | while read line ; do
-				if [ "$(echo $line | grep $apid)" ] ;then
+				if [ "$(echo $line | grep $cf_apid)" ] ;then
 					#Update License Key
 					uci set wifimedia.@advance[0].wfm="$(cat /etc/opt/license/wifimedia)"
 					cat /etc/opt/license/wifimedia >/etc/opt/license/status
