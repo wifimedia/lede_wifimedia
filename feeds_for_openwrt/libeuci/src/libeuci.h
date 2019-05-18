7db0ad898217c7fc6d43b9c73c5794d71547dead
wifimedia <dungtd8x@gmail.com>
2019-05-18 09:28:53 +0700

    .....

--- feeds_for_openwrt/libeuci/src/libeuci.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIBEUCI_H
-#define LIBEUCI_H
-
-#include <uci.h>
-
-int euci_get(struct uci_context *ctx, const char *config, const char *section, const char *type, const char *option, char *out, int olen);
-
-#endif
--- feeds_for_openwrt/lua-daemon/src/test.lua
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/lua
-
-local daemon = require "daemon"
-
-print(daemon.daemonize(true))
-
-while true do
-	os.execute("sleep 1")
-end
