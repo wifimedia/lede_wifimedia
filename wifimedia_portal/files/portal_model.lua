2a72e23d9b69e23da5fbf19d2ca45f80ddcc03e6
wifimedia <dungtd8x@gmail.com>
2019-05-21 12:01:13 +0700

    ..

--- wifimedia_portal/files/portal_model.lua
+++ wifimedia_portal/files/portal_model.lua
@@ -12,6 +12,7 @@ m = Map("wifimedia",translate(""))
 m.apply_on_parse = true
 function m.on_apply(self)
 		luci.util.exec("/sbin/wifimedia/preauthenticated_rules.sh >/dev/null")
+		luci.util.exec("/sbin/wifimedia/preauthenticated_rules.sh next_net >/dev/null")
 		--luci.util.exec("sleep 15 && reboot >/dev/null")
 end
 
