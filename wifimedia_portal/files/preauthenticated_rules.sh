1eccbb1a6f3f495a8fdef4be0307a6a41a16f8bb
wifimedia <dungtd8x@gmail.com>
2019-08-07 15:07:44 +0700

    update facebook

--- wifimedia_portal/files/preauthenticated_rules.sh
+++ wifimedia_portal/files/preauthenticated_rules.sh
@@ -49,7 +49,7 @@ uci set nodogsplash.@nodogsplash[0].authidletimeout="$authidletimeout_default";
 uci set nodogsplash.@nodogsplash[0].sessiontimeout="$sessiontimeout_default";
 uci set nodogsplash.@nodogsplash[0].checkinterval="$ctv";
 # Whitelist IP
-for i in portal.nextify.vn static.nextify.vn nextify.vn crm.nextify.vn $walledgadent; do
+for i in portal.nextify.vn static.nextify.vn nextify.vn crm.nextify.vn facebook.com facebook.net fbcdn.net fbsbx.com  $walledgadent; do
     nslookup ${i} 8.8.8.8 2> /dev/null | \
         grep 'Address ' | \
         grep -v '127\.0\.0\.1' | \
