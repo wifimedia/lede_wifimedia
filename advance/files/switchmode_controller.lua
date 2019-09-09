4ec493a0c2e05a3900ded18d80ddd951777cbb0b
wifimedia <dungtd8x@gmail.com>
2019-09-09 11:32:11 +0700

    fix ethernet switch all port lan & wan

---
+++ advance/files/switchmode_controller.lua
@@ -0,0 +1,9 @@
+--[[
+LuCI - Lua Configuration Interface
+Copyright 2014 dungtd8x <dungtd8x@gmail.com>
+]]--
+module("luci.controller.wifimedia.switchmode", package.seeall)
+function index()
+	--entry( { "admin", "wifimedia"}, firstchild(), "Wifimedia", 50).dependent=false
+	entry( { "admin", "wifimedia", "switchmode" }, cbi("wifimedia_module/switchmode"), _("Ethernet switch "),      15)
+end
