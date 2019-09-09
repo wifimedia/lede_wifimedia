e135f403d24eebffecc63865d3844a216458a3a1
wifimedia <dungtd8x@gmail.com>
2019-09-09 11:00:15 +0700

    fix switch mode ethernet

--- advance/files/switchmode_controller.lua
+++ advance/files/switchmode_controller.lua
@@ -2,8 +2,8 @@
 LuCI - Lua Configuration Interface
 Copyright 2014 dungtd8x <dungtd8x@gmail.com>
 ]]--
-module("luci.controller.wifimedia.bridge", package.seeall)
+module("luci.controller.wifimedia.switchmode", package.seeall)
 function index()
 	--entry( { "admin", "wifimedia"}, firstchild(), "Wifimedia", 50).dependent=false
-	entry( { "admin", "wifimedia", "bridge" }, cbi("wifimedia_module/bridge"), _("Ethernet switch "),      15)
+	entry( { "admin", "wifimedia", "switchmode" }, cbi("wifimedia_module/switchmode"), _("Ethernet switch "),      15)
 end
