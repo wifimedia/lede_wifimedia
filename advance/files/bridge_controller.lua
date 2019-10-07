38ebf58a174cc291f48410885f07ede746649fde
wifimedia <dungtd8x@gmail.com>
2019-10-07 09:46:55 +0700

    ...

--- advance/files/bridge_controller.lua
+++ advance/files/bridge_controller.lua
@@ -4,6 +4,5 @@ Copyright 2014 dungtd8x <dungtd8x@gmail.com>
 ]]--
 module("luci.controller.wifimedia.bridge", package.seeall)
 function index()
-	--entry( { "admin", "wifimedia"}, firstchild(), "Wifimedia", 50).dependent=false
 	entry( { "admin", "wifimedia", "bridge" }, cbi("wifimedia_module/bridge"), _("Bridge Network"),      15)
 end
