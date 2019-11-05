a269989647055d77e83a273e24615736fd7448e5
wifimedia <dungtd8x@gmail.com>
2019-11-05 14:29:36 +0700

    global_device=`ifconfig eth0 | grep 'HWaddr' | awk '{ print $5 }' | sed 's/:/-/g' | tr a-z A-Z` #Detect Client Connect Nextify

--- advance/files/variables.sh
+++ advance/files/variables.sh
@@ -68,7 +68,8 @@ find_mac_gateway="/tmp/mac_gateway"
 #cf_apid=$(echo $cf_device | sed 's/:/-/g')
 wr940_device=`ifconfig eth0 | grep 'HWaddr' | awk '{ print $5 }' | sed 's/:/-/g' | tr a-z A-Z`
 #wr940_apid=$(echo $cf_device | sed 's/:/-/g')
-global_device=`ifconfig eth0 | grep 'HWaddr' | awk '{ print $5 }'`
+#global_device=`ifconfig eth0 | grep 'HWaddr' | awk '{ print $5 }'`
+global_device=`ifconfig eth0 | grep 'HWaddr' | awk '{ print $5 }' | sed 's/:/-/g' | tr a-z A-Z` #Detect Client Connect Nextify
 #--------------RSSI------------------------------
 rssi_on=$(uci -q get wifimedia.@advance[0].enable)
 #------------------------------------------------
