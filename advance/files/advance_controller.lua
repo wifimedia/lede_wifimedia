7041f8e20c5110456f665cd4c3c92fea5b20f267
wifimedia <dungtd8x@gmail.com>
2019-11-08 22:53:36 +0700

    remove info

--- advance/files/advance_controller.lua
+++ advance/files/advance_controller.lua
@@ -6,5 +6,5 @@ module("luci.controller.wifimedia.advance", package.seeall)
 function index()
 	entry( { "admin", "wifimedia"}, firstchild(), "Service", 50).dependent=false
 	entry( { "admin", "wifimedia", "advance" }, cbi("wifimedia_module/advance"), _("Advanced"),      10)
-	entry( { "admin", "wifimedia", "info"    }, template("wifimedia_view/index"),    _("Info"), 80)
+	--entry( { "admin", "wifimedia", "info"    }, template("wifimedia_view/index"),    _("Info"), 80)
 end
