b8566fab4e9b9f5a5428a133c5cd407537b97601
wifimedia <dungtd8x@gmail.com>
2019-11-11 16:14:26 +0700

    Captive Portal

--- wifimedia_portal/files/nodogsplash_preauth.sh
+++ wifimedia_portal/files/nodogsplash_preauth.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 CLIENT_MAC=${2}
-#GATEWAY_MAC=$(ifconfig | grep br-lan | grep HWaddr | tr -s ' ' | cut -d' ' -f5)
-GATEWAY_MAC=$(cat /sys/class/ieee80211/phy0/macaddress | tr a-z A-Z)
+GATEWAY_MAC=$(ifconfig | grep br-lan | grep HWaddr | tr -s ' ' | cut -d' ' -f5)
+#GATEWAY_MAC=$(cat /sys/class/ieee80211/phy0/macaddress | tr a-z A-Z)
 
 # Gửi thông tin lên server
 # Do có nhiều clients ở trạng thái preauthenticated, gửi lên theo cách này thống kê được nhiều hơn
