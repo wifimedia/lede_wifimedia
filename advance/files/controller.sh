804de604e1a28c561338e473e49ec036d5f41922
wifimedia <dungtd8x@gmail.com>
2019-11-23 14:19:46 +0700

    fix lic

--- advance/files/controller.sh
+++ advance/files/controller.sh
@@ -44,9 +44,10 @@ license_srv() {
 					cat /etc/opt/license/wifimedia >/etc/opt/license/status
 					license_local
 				else
-					#echo "we will maintain the existing settings."
-					#echo "Wrong License Code & auto reboot" >/etc/opt/license/status
-					#enable cronjob chek key
+					uci set wireless.radio0.disabled="1"
+					uci set wireless.radio1.disabled="1"
+					uci commit wireless
+					wifi
 					echo "0 0 * * * /sbin/wifimedia/controller.sh license_srv" > /etc/crontabs/wificode
 					#/etc/init.d/cron restart
 				fi
