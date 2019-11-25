6baf1a85cb9004dd42d4d1524f534ce8ebc03bc1
wifimedia <dungtd8x@gmail.com>
2019-11-25 11:40:43 +0700

    config build

--- advance/files/controller.sh
+++ advance/files/controller.sh
@@ -243,52 +243,6 @@ if [ "$uptime" -gt 15 ]; then #>15days
 fi
 }
 
-eap_manager() {
-
-rm -f /tmp/eap_mac
-rm -f /tmp/eap
-cat "$eap_device" | while read line ; do
-	mac=$(echo $line | awk '{print $2}'| tr '[a-z]' '[A-Z]' | cut -d ':' -f1-5)
-	maclast=$(echo $line | awk '{print $2}'| tr '[a-z]' '[A-Z]' | cut -d ':' -f6)
-	#echo $maclast
-	zero=$(echo $maclast | cut -c 1)
-	echo $zero
-	#echo "Mac address= $mac:$maclast"
-
-	decmac=$(echo "ibase=16; $maclast"|bc)
-	if [ $decmac -eq '241' ]
-	then
-	macinc='00'
-	else
-	incout=`expr $decmac + 1 `
-	macinc=$(echo "obase=16; $incout"|bc)
-
-	fi
-		
-	if [ $zero -eq '0' ];then
-		#echo "Mac address= $mac:$zero$macinc"
-		echo "$mac:$zero$macinc" >>/tmp/eap_mac
-	else
-		#echo "Mac address= $mac:$macinc"
-		echo "$mac:$macinc" >>/tmp/eap_mac
-	fi
-done
-#EXPORT DATA AP IP MAC
-cat "/tmp/eap_mac" | while read line ; do
-
-	#linedeap=$(echo $line | awk '{print $1}' | sed 's/-/:/g' | tr A-Z a-z)
-	#arp | grep $linedeap | awk '{print $4 " "$1 " http://" $1 }' >>/tmp/eap
-	#echo $linedeap
-	eapmac=$(echo $line | awk '{print $1}' | sed 's/-/:/g' | tr A-Z a-z)
-	cat "/proc/net/arp" | while read line ; do
-		arpmac=$(echo $line | awk '{print $4}' | sed 's/-/:/g' )
-		if [ "$eapmac" == "$arpmac" ] ;then
-			echo $line | awk '{print $4 " "$1 " http://" $1 }' >>/tmp/eap
-		fi
-	done	
-done
-}
-
 disable_3_port(){
 	for i in 2 3 4; do
 		swconfig dev switch0 port $i set disable 1
