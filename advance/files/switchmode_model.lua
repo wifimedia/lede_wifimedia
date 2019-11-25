dd6ff3ddc97316a48cd6e8adda3f042a7f749f2c
wifimedia <dungtd8x@gmail.com>
2019-11-25 11:47:29 +0700

    add

--- advance/files/switchmode_model.lua
+++ advance/files/switchmode_model.lua
@@ -9,22 +9,21 @@ local uci = require "luci.model.uci".cursor()
 m = Map("wifimedia", "")
 m.apply_on_parse = true
 function m.on_apply(self)
-	--luci.sys.call("env -i /bin/ubus call network reload >/dev/null 2>/dev/null")
-	luci.sys.call("env -i /bin/ubus call network restart >/dev/null 2>/dev/null")
+	luci.sys.call("env -i /bin/ubus call network reload >/dev/null 2>/dev/null")
 	--luci.http.redirect(luci.dispatcher.build_url("admin","wifimedia","advance"))
 end
 
-s = m:section(TypedSection, "switchmode","Ethernet Switch")
+s = m:section(TypedSection, "advance","Ethernet Switch")
 s.anonymous = true
 s.addremove = false
 
-switch_mode = s:option(Flag, "switch_port","All port LAN & WAN","")
-switch_mode.rmempty = false
-		function switch_mode.write(self, section, value)
+bridge_mode = s:option(Flag, "bridge_mode","Bridge","")
+bridge_mode.rmempty = false
+		function bridge_mode.write(self, section, value)
 			if value == self.enabled then
 				luci.sys.call("uci delete network.lan")
 				luci.sys.call("uci set network.wan.proto='dhcp'")
-				luci.sys.call("uci set network.wan.ifname='eth0.1 eth0.2'")
+				luci.sys.call("uci set network.wan.ifname='eth0 eth1'")
 				luci.sys.call("uci set wireless.@wifi-iface[0].network='wan'")
 				luci.sys.call("uci commit")
 			else
@@ -33,16 +32,16 @@ switch_mode.rmempty = false
 				luci.sys.call("uci set network.lan.ipaddr='172.16.99.1'")
 				luci.sys.call("uci set network.lan.netmask='255.255.255.0'")
 				luci.sys.call("uci set network.lan.type='bridge'")
-				luci.sys.call("uci set network.lan.ifname='eth0.1'")
+				luci.sys.call("uci set network.lan.ifname='eth1'")
 				luci.sys.call("uci set dhcp.lan.force='1'")
 				luci.sys.call("uci set dhcp.lan.netmask='255.255.255.0'")
 				luci.sys.call("uci del dhcp.lan.dhcp_option")
 				luci.sys.call("uci add_list dhcp.lan.dhcp_option='6,8.8.8.8,8.8.4.4'")				
-				luci.sys.call("uci set network.wan.ifname='eth0.2'")
+				luci.sys.call("uci set network.wan.ifname='eth0'")
 				luci.sys.call("uci set wireless.@wifi-iface[0].network='wan'")
 				luci.sys.call("uci commit")		
 			end
 			return Flag.write(self, section, value)
 		end
-		function switch_mode.remove() end
+		function bridge_mode.remove() end
 return m
