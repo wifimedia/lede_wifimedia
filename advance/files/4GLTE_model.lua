852cc36355fd94abbae58813c82db7fd01ab4716
wifimedia <dungtd8x@gmail.com>
2019-11-28 08:52:17 +0700

    add option 4G LTE

--- advance/files/4GLTE_model.lua
+++ advance/files/4GLTE_model.lua
@@ -14,33 +14,22 @@ function m.on_apply(self)
 	--luci.http.redirect(luci.dispatcher.build_url("admin","wifimedia","advance"))
 end
 
-s = m:section(TypedSection, "4G-LTE","Ethernet Switch")
+s = m:section(TypedSection, "LTE","4G LTE Interface")
 s.anonymous = true
 s.addremove = false
 
-lte = s:option(Flag, "4g-lte","4G LTE Interface","")
+lte = s:option(Flag, "4g-lte","4G LTE Enable ")
 lte.rmempty = false
 		function lte.write(self, section, value)
-			if value == self.enabled then
-				luci.sys.call("uci delete network.lan")
-				luci.sys.call("uci set network.wan.proto='dhcp'")
-				luci.sys.call("uci set network.wan.ifname='eth0.1 eth0.2'")
-				luci.sys.call("uci set wireless.@wifi-iface[0].network='wan'")
+			if value == self.enabled then			
+			    luci.sys.call("uci set network.lte='interface'")
+				luci.sys.call("uci set network.lan.proto='dhcp'")
+				luci.sys.call("uci set network.lan.type='bridge'")
+				luci.sys.call("uci set network.lan.ifname='eth1'")
 				luci.sys.call("uci commit")
 			else
-			    luci.sys.call("uci set network.lan='interface'")
-				luci.sys.call("uci set network.lan.proto='static'")
-				luci.sys.call("uci set network.lan.ipaddr='172.16.99.1'")
-				luci.sys.call("uci set network.lan.netmask='255.255.255.0'")
-				luci.sys.call("uci set network.lan.type='bridge'")
-				luci.sys.call("uci set network.lan.ifname='eth0.1'")
-				luci.sys.call("uci set dhcp.lan.force='1'")
-				luci.sys.call("uci set dhcp.lan.netmask='255.255.255.0'")
-				luci.sys.call("uci del dhcp.lan.dhcp_option")
-				luci.sys.call("uci add_list dhcp.lan.dhcp_option='6,8.8.8.8,8.8.4.4'")				
-				luci.sys.call("uci set network.wan.ifname='eth0.2'")
-				luci.sys.call("uci set wireless.@wifi-iface[0].network='wan'")
-				luci.sys.call("uci commit")		
+				luci.sys.call("uci delete network.lan")
+				luci.sys.call("uci commit")
 			end
 			return Flag.write(self, section, value)
 		end
