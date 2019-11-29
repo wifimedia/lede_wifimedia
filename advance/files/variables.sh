71fac36393d5a9b57be87da2da63069c406e5871
wifimedia <dungtd8x@gmail.com>
2019-11-29 21:36:15 +0700

    add ip lan, ip wan

--- advance/files/variables.sh
+++ advance/files/variables.sh
@@ -27,6 +27,11 @@ hash256=$(sha256sum $response_file | awk '{print $1}')
 device_fw=$(cat /sys/class/ieee80211/phy0/macaddress |sed 's/:/-/g' | tr a-z A-Z)
 link_config=`uci -q get wifimedia.@server[0].links`
 cpn_url=`uci -q get wifimedia.@nodogsplash[0].cpnurl`
+
+ip_wan=$(ifconfig br-wan | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1 }')
+ip_lan=$(ifconfig br-lan | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1 }')
+#ip_gateway=$(route -n | grep 'UG' | grep 'br-wan' | awk '{ print $2 }')
+
 #echo "Waiting a bit..."
 #sleep $(head -30 /dev/urandom | tr -dc "0123456789" | head -c1)
 if [ ! -d "/tmp/upgrade" ]; then mkdir /tmp/upgrade; fi
