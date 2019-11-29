70c1d7f36b4b51b2048c2b9cbaaa1078653f9801
wifimedia <dungtd8x@gmail.com>
2019-11-29 14:36:09 +0700

    fix url link

--- advance/files/variables.sh
+++ advance/files/variables.sh
@@ -26,7 +26,7 @@ response_file=/tmp/_cfg
 hash256=$(sha256sum $response_file | awk '{print $1}')
 device_fw=$(cat /sys/class/ieee80211/phy0/macaddress |sed 's/:/-/g' | tr a-z A-Z)
 link_config=`uci -q get wifimedia.@server[0].links`
-cpn_url=`uci -q get wifimedia.@nodogsplash[0].server`
+cpn_url=`uci -q get wifimedia.@nodogsplash[0].cpnurl`
 #echo "Waiting a bit..."
 #sleep $(head -30 /dev/urandom | tr -dc "0123456789" | head -c1)
 if [ ! -d "/tmp/upgrade" ]; then mkdir /tmp/upgrade; fi
