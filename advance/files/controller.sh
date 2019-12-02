031b8c5180b128375429d96f1e9be4607a37d808
wifimedia <dungtd8x@gmail.com>
2019-12-02 12:05:21 +0700

    fix add

--- advance/files/controller.sh
+++ advance/files/controller.sh
@@ -29,6 +29,265 @@ checking (){
 	#if [ -z $pidhostapd ];then echo "Wireless Off" >/tmp/wirelessstatus;else echo "Wireless On" >/tmp/wirelessstatus;fi
 }
 
+device_cfg(){
+	#wget -q "${cfg_ctl}" -O $response_file
+	#response_file=/tmp/_cfg
+	#hash256=$(sha256sum $response_file | awk '{print $1}')
+	if [ "$(uci -q get wifimedia.@hash256[0].value)" != "$hash256" ]; then
+		start_cfg
+		/etc/init.d/network restart
+	fi
+	
+	monitor_port
+	#get_client_connect_wlan $cpn_url
+	
+	uci set wifimedia.@hash256[0].value=$hash256
+	wget --post-data="gateway_mac=${global_device}&isp=${PUBLIC_IP}&ip_wan=${ip_wan}&ip_lan=${ip_lan}&diagnostics=${diagnostics}&ports_data=${ports_data}" $link_config -O /tmp/device_cfg
+	rm /tmp/monitor_port
+}
+
+start_cfg(){
+
+local key
+local value
+cat $response_file | while read line ; do
+	key=$(echo "$line" | cut -f 1 -d =)
+	value=$(echo "$line" | cut -f 2- -d =)
+	
+	#Cau hinh hostname
+	if [ "$key" = "device.hostname" ];then
+		uci set system.@system[0].hostname="$value"
+	#Mat khau thiet bi	
+	elif [ "$key" = "device.passwd" ];then
+		echo -e "$value\n$value" | passwd root
+	#Reboot device	
+	elif [ "$key" = "device.reboot" ];then
+		echo $value >tmp/reboot_flag
+	#Cau hinh wireless
+	elif [ "$key" = "wireless.radio2G.enable" ];then
+		uci set wireless.radio0.disabled="$value"
+	elif [ "$key" = "wireless.radio2G.channel" ];then
+		uci set wireless.radio0.channel="$value"
+	elif [ "$key" = "wireless.radio2G.htmode" ];then
+		uci set wireless.radio0.htmode="$value"
+	elif [ "$key" = "wireless.radio2G.txpower" ];then
+		uci set wireless.radio0.txpower="$value"
+	elif [ "$key" = "wireless.ssid2G" ];then
+		uci set wireless.default_radio0.ssid="$value"
+	elif [ "$key" = "wireless.passwd2G" ];then
+		if [ "$value" = "" ];then
+			uci set wireless.default_radio0.key=""
+			uci set wireless.default_radio0.encryption="none"
+		else
+			uci set wireless.default_radio0.key="$value"
+			uci set wireless.default_radio0.encryption="psk2"
+		fi
+	#chuyen dung chuan cache	
+	elif [ "$key" = "wireless.okc2G=" ];then
+		if [ "$value" =  "1" ];then
+			uci set wireless.default_radio0.rsn_preauth="$value"
+			uci set wireless.default_radio0.ieee80211r ="0"
+			uci set wireless.default_radio0.ft_over_ds="0"
+			uci set wireless.default_radio0.ft_psk_generate_local="0"
+		fi	
+	#Chuyen dung 802.1R	
+	elif [ "$key" = "wireless.ft2G" ];then
+		if [ "$value" =  "1" ];then
+			uci set wireless.default_radio0.rsn_preauth="0"
+			uci set wireless.default_radio0.ieee80211r ="1"
+			uci set wireless.default_radio0.ft_over_ds="1"
+			uci set wireless.default_radio0.ft_psk_generate_local="1"
+		fi	
+	##Map SSID to net/plain LAN or WAN	
+	elif [ "$key" = "wireless.network2G" ];then
+		uci set wireless.default_radio0.network="$value"
+	#Set Max Client	
+	elif [ "$key" = "wireless.maxclients2G" ];then
+		uci set wireless.default_radio0.maxassoc="$value"
+	
+	##Cau hinh switch 5 port		
+	elif [ "$key" = "network.switch" ];then
+		echo $value >tmp/switch_flag
+		if [ "$value" = "1" ];then
+			uci delete network.lan
+			uci set network.wan.proto="dhcp"
+			uci set network.wan.ifname="eth0 eth1.1"
+			uci set wireless.@wifi-iface[0].network="wan"
+			uci set wifimedia.@switchmode[0].switch_port="$value"
+			uci commit
+		else
+			uci set network.lan="interface"
+			uci set network.lan.proto="static"
+			uci set network.lan.ipaddr="172.16.99.1"
+			uci set network.lan.netmask="255.255.255.0"
+			uci set network.lan.type="bridge"
+			uci set network.lan.ifname="eth1.1"
+			uci set dhcp.lan.force="1"
+			uci set dhcp.lan.netmask="255.255.255.0"
+			uci del dhcp.lan.dhcp_option
+			uci add_list dhcp.lan.dhcp_option="6,8.8.8.8,8.8.4.4"				
+			uci set network.wan.ifname="eth0"
+			uci set wireless.@wifi-iface[0].network="wan"
+			uci set wifimedia.@switchmode[0].switch_port="0"
+			uci commit			
+		fi
+	#Cu hinh IP LAN/WAN
+	elif [ "$key" = "network.lan.static" ];then
+		if [ "$value" = "1" ];then ##Static 
+			uci set network.lan="interface"
+			uci set network.lan.proto="static"
+			uci set network.lan.type="bridge"
+			uci set network.lan.ifname="eth1.1"		
+		else ##DHCP Client nhan IP
+			uci delete network.lan
+			uci set network.lan.proto="dhcp"
+			uci set network.lan.ifname="eth1.1"		
+		fi
+	elif [  "$key" = "network.lan.ip" ];then
+		uci set network.lan.ipaddr="$value"
+	elif [  "$key" = "network.lan.subnetmask" ];then
+		uci set network.lan.netmask="$value"
+	elif [  "$key" = "network.lan.gateway" ];then
+		uci set network.lan.gateway="$value"		
+	elif [  "$key" = "network.lan.dns" ];then
+		value=$(echo $value | sed 's/,/ /g')
+		uci set network.lan.dns="$value"		
+	###WAN config
+	elif [ "$key" = "network.wan.static" ];then
+		if [ "$value" = "1" ];then ##Static 
+			uci set network.wan="interface"
+			uci set network.wan.proto="static"
+			uci set network.wan.type="bridge"
+			uci set network.wan.ifname="eth1"		
+		else ##DHCP Client nhan IP
+			uci delete network.wan
+			uci set network.wan.proto="dhcp"
+			uci set network.wan.ifname="eth1"		
+		fi
+	elif [  "$key" = "network.lan.ip" ];then
+		uci set network.lan.ipaddr="$value"
+	elif [  "$key" = "network.lan.subnetmask" ];then
+		uci set network.lan.netmask="$value"
+	elif [  "$key" = "network.lan.gateway" ];then
+		uci set network.lan.gateway="$value"		
+	elif [  "$key" = "network.lan.dns" ];then
+		value=$(echo $value | sed 's/,/ /g')
+		uci set network.lan.dns="$value"		
+	##Cau hinh DHCP
+	elif [  "$key" = "lan.dhcp.start" ];then
+		uci set dhcp.lan.start="$value"
+	elif [  "$key" = "lan.dhcp.limit" ];then
+		uci set dhcp.lan.limit="$value"
+	elif [  "$key" = "lan.dhcp.leasetime" ];then
+		uci set dhcp.lan.leasetime="$value"
+		
+	#Cau hinh Captive Portal
+	elif [  "$key" = "cpn.enable" ];then
+		echo $value >tmp/cpn_flag
+		uci set nodogsplash.@nodogsplash[0].enabled="$value"
+	elif [  "$key" = "cpn.domain" ];then
+		uci set wifimedia.@nodogsplash[0].domain="$value"
+	elif [  "$key" = "cpn.walledgarden" ];then
+		value=$(echo $value | sed 's/,/ /g')
+		uci set wifimedia.@nodogsplash[0].preauthenticated_users="$value"
+	elif [  "$key" = "cpn.fb" ];then
+		uci set wifimedia.@nodogsplash[0].facebook="$value"
+	elif [  "$key" = "cpn.dhcpextenal" ];then
+		uci set wifimedia.@nodogsplash[0].dhcpextension="$value"
+	elif [  "$key" = "cpn.clientdetect" ];then
+		uci set wifimedia.@nodogsplash[0].cpn="$value"
+		echo $value >tmp/clientdetect
+	#Cau hinh auto reboot
+	elif [  "$key" = "scheduletask.enable" ];then
+		echo $value >tmp/scheduled_flag
+	elif [  "$key" = "scheduletask.hours" ];then
+		uci set scheduled.@times[0].hour="$value"
+	elif [  "$key" = "scheduletask.minute" ];then
+		uci set scheduled.@times[0].minute="$value"
+	fi
+##
+done	
+uci commit
+if [ $(cat /tmp/reboot_flag) -eq 1 ]; then
+	echo "restarting the node"
+	reboot
+fi
+
+if [ $(cat /tmp/>tmp/cpn_flag) -eq 1 ]; then
+	echo "Config & Start CPN" 
+	/sbin/wifimedia/captive_portal.sh config_captive_portal
+	echo '*/5 * * * * /sbin/wifimedia/captive_portal.sh heartbeat'>/etc/crontabs/nds
+	/etc/init.d/cron restart
+else
+  echo "Stop CPN"
+  /etc/init.d/nodogsplash stop
+fi
+if [ $(cat tmp/cpn_flag) -eq 1 ]; then
+	echo "restarting conjob"
+	crontab /etc/cron_nds -u nds && /etc/init.d/cron restart
+fi		
+}
+
+monitor_port(){
+swconfig dev switch0 show |  grep 'link'| awk '{print $2, $3}' | while read line;do
+	echo "$line," >>/tmp/monitor_port
+done
+ports_data==$(cat /tmp/monitor_port | xargs| sed 's/,/;/g')
+echo $ports_data
+#wget --post-data="gateway_mac=${global_device}&ports_data=${ports_data}" $link_config -O /dev/null
+#rm /tmp/monitor_port
+}
+_detect_clients(){
+	get_client_connect_wlan $cpn_url
+}
+
+##Sent Client MAC to server Nextify
+get_client_connect_wlan(){
+    #ip_opvn=`ifconfig tun0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1 }'`
+	local _url=$1
+	NEWLINE_IFS='
+'
+	OLD_IFS="$IFS"; IFS=$NEWLINE_IFS
+	signal=''
+	host=''
+	mac=''
+	touch /tmp/client_connect_wlan
+	for iface in `iw dev | grep Interface | awk '{print $2}'`; do
+		for line in `iwinfo $iface assoclist`; do
+			if echo "$line" | grep -q "SNR"; then
+				if [ -f /etc/ethers ]; then
+					mac=$(echo $line | awk '{print $1}' FS=" ")
+					host=$(awk -v MAC=$mac 'tolower($1)==MAC {print $1}' FS=" " /etc/ethers)
+					data=";$mac"
+					echo $data >>/tmp/client_connect_wlan
+				fi
+			fi
+		done
+	done
+	IFS="$OLD_IFS"
+	client_connect_wlan=$(cat /tmp/client_connect_wlan | xargs| sed 's/;//g'| tr a-z A-Z)
+	number_client=$(cat /tmp/client_connect_wlan | wc -l)
+	#monitor_port
+	wget --post-data="&access_point_macs=${global_device}&mac_clients=${client_connect_wlan}&clients=${clients}" $cpn_url -O /dev/null #https://api.telitads.vn/v1/access_points/state
+	#wget --post-data="clients=${client_connect_wlan}&gateway_mac=${global_device}&number_client=${number_client}" $_url -O /dev/null #http://api.nextify.vn/clients_around
+	echo $client_connect_wlan
+	rm /tmp/client_connect_wlan
+}
+
+action_lan_wlan(){
+	echo "" > $find_mac_gateway
+	wget -q "${blacklist}" -O $find_mac_gateway
+	curl_result=$?
+	if [ "${curl_result}" -eq 0 ]; then
+		cat "$find_mac_gateway" | while read line ; do
+			if [ "$(echo $line | grep $_device)" ] ;then
+				wifi down
+				ifdown lan
+			fi
+		done	
+	fi
+}
+
 license_srv() {
 	###MAC WAN:WR940NV6 --Ethernet0 OPENWRT19
 	echo "" > $licensekey
@@ -37,18 +296,13 @@ license_srv() {
 	if [ "${curl_result}" -eq 0 ]; then
 		if grep -q "." $licensekey; then
 			cat "$licensekey" | while read line ; do
-				if [ "$(echo $line | grep $wr940_device)" ] ;then
-					#Update License Key
+				if [ "$(echo $line | grep $_device)" ] ;then
 					uci set wifimedia.@wireless[0].wfm="$(cat /etc/opt/license/wifimedia)"
 					uci commit wifimedia
 					cat /etc/opt/license/wifimedia >/etc/opt/license/status
 					license_local
 				else
-					#echo "we will maintain the existing settings."
-					#echo "Wrong License Code & auto reboot" >/etc/opt/license/status
-					#enable cronjob chek key
 					echo "0 0 * * * /sbin/wifimedia/controller.sh license_srv" > /etc/crontabs/wificode
-					#/etc/init.d/cron restart
 				fi
 			done	
 		fi
@@ -103,106 +357,6 @@ license_local() {
 	fi
 }
 
-eap_manager() {
-	rm -f /tmp/eap_mac
-	rm -f /tmp/eap
-	cat "$eap_device" | while read line ; do
-		mac=$(echo $line | awk '{print $2}'| tr '[a-z]' '[A-Z]' | cut -d ':' -f1-5)
-		maclast=$(echo $line | awk '{print $2}'| tr '[a-z]' '[A-Z]' | cut -d ':' -f6)
-		#echo $maclast
-		zero=$(echo $maclast | cut -c 1)
-		echo $zero
-		#echo "Mac address= $mac:$maclast"
-
-		decmac=$(echo "ibase=16; $maclast"|bc)
-		if [ $decmac -eq '241' ]
-		then
-		macinc='00'
-		else
-		incout=`expr $decmac + 1 `
-		macinc=$(echo "obase=16; $incout"|bc)
-
-		fi
-			
-		if [ $zero -eq '0' ];then
-			#echo "Mac address= $mac:$zero$macinc"
-			echo "$mac:$zero$macinc" >>/tmp/eap_mac
-		else
-			#echo "Mac address= $mac:$macinc"
-			echo "$mac:$macinc" >>/tmp/eap_mac
-		fi
-	done
-	#EXPORT DATA AP IP MAC
-	cat "/tmp/eap_mac" | while read line ; do
-
-		#linedeap=$(echo $line | awk '{print $1}' | sed 's/-/:/g' | tr A-Z a-z)
-		#arp | grep $linedeap | awk '{print $4 " "$1 " http://" $1 }' >>/tmp/eap
-		#echo $linedeap
-		eapmac=$(echo $line | awk '{print $1}' | sed 's/-/:/g' | tr A-Z a-z)
-		cat "/proc/net/arp" | while read line ; do
-			arpmac=$(echo $line | awk '{print $4}' | sed 's/-/:/g' )
-			if [ "$eapmac" == "$arpmac" ] ;then
-				echo $line | awk '{print $4 " "$1 " http://" $1 }' >>/tmp/eap
-			fi
-		done	
-	done
-}
-
-action_lan_wlan(){
-	echo "" > $find_mac_gateway
-	wget -q "${blacklist}" -O $find_mac_gateway
-	curl_result=$?
-	if [ "${curl_result}" -eq 0 ]; then
-		cat "$find_mac_gateway" | while read line ; do
-			if [ "$(echo $line | grep $wr940_device)" ] ;then
-				wifi down
-				ifdown lan
-			fi
-		done	
-	fi
-}
-
-monitor_port(){
-swconfig dev switch0 show |  grep 'link'| awk '{print $2, $3}' | while read line;do
-	echo "$line," >>/tmp/monitor_port
-done
-ports_data==$(cat /tmp/monitor_port | xargs| sed 's/,/;/g')
-echo $ports_data
-wget --post-data="gateway_mac=${global_device}&ports_data=${ports_data}" $link_post -O /dev/null
-rm /tmp/monitor_port
-}
-
-##Sent Client MAC to server Nextify
-get_client_connect_wlan(){
-    #ip_opvn=`ifconfig tun0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1 }'`
-	NEWLINE_IFS='
-'
-	OLD_IFS="$IFS"; IFS=$NEWLINE_IFS
-	signal=''
-	host=''
-	mac=''
-	touch /tmp/client_connect_wlan
-	for iface in `iw dev | grep Interface | awk '{print $2}'`; do
-		for line in `iwinfo $iface assoclist`; do
-			if echo "$line" | grep -q "SNR"; then
-				if [ -f /etc/ethers ]; then
-					mac=$(echo $line | awk '{print $1}' FS=" ")
-					host=$(awk -v MAC=$mac 'tolower($1)==MAC {print $1}' FS=" " /etc/ethers)
-					data=";$mac"
-					echo $data >>/tmp/client_connect_wlan
-				fi
-			fi
-		done
-	done
-	IFS="$OLD_IFS"
-	client_connect_wlan=$(cat /tmp/client_connect_wlan | xargs| sed 's/;//g'| tr a-z A-Z)
-	clients=$(cat /tmp/client_connect_wlan | wc -l)
-	#monitor_port
-	wget --post-data="&access_point_macs=${global_device}&mac_clients=${client_connect_wlan}&clients=${clients}" $cpn_url -O /dev/null #https://api.telitads.vn/v1/access_points/state
-	echo $client_connect_wlan
-	rm /tmp/client_connect_wlan
-}
-
 rssi() {
 if [ $rssi_on == "1" ];then
 	level_defaults=-80
@@ -245,6 +399,30 @@ if [ $rssi_on == "1" ];then
 	echo "#!/bin/sh" >/tmp/denyclient
 fi #END RSSI
 
+}
+
+openvpn(){
+cfg_ovpn=/etc/openvpn/wifimedia.ovpn
+srv_ovpn="http://openvpn.wifimedia.vn/$_device.ovpn"
+certificate=wifimedia
+uci batch <<-EOF
+	set openvpn.${certificate}=openvpn
+	set openvpn.${certificate}.config="$cfg_ovpn"
+	set openvpn.${certificate}.enabled="1"
+	commit openvpn
+EOF
+
+	wget -q "${srv_ovpn}" -O $cfg_ovpn
+	curl_result=$?
+	if [ "${curl_result}" -eq 0 ]; then
+		uci set openvpn.${certificate}.enabled="1"
+		uci commit openvpn
+		/etc/init.d/openvpn start ${certificate}
+	else
+		uci set openvpn.${certificate}.enabled="1"
+		uci commit openvpn
+		/etc/init.d/openvpn stop ${certificate}
+	fi
 }
 
 "$@"
