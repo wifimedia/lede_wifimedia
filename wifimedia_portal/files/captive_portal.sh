cbc6327b89d2671a25afbb62b6d8281fc6e25138
wifimedia <dungtd8x@gmail.com>
2019-12-11 16:49:31 +0700

    fix update

--- wifimedia_portal/files/captive_portal.sh
+++ wifimedia_portal/files/captive_portal.sh
@@ -161,76 +161,7 @@ heartbeat(){
 	wget -q --timeout=3 \
 		 "http://portal.nextify.vn/heartbeat?mac=${MAC}&uptime=${UPTIME}&num_clients=${NUM_CLIENTS}&total_clients=${TOTAL_CLIENTS}" \
 		 -O /tmp/config_setting
-	get_config	 
 	captive_portal_restart
-}
-
-setting_config() {
-	local network_1=$(uci -q get wireless.default_radio0.network)
-	local network_2=$(uci -q get wireless.default_radio1.network)
-	local md5ndsconfig=`uci -q get wifimedia.@nodogsplash[0].md5sum`
-	local checkmd5file=`md5sum /tmp/config_setting | awk '{print $1}'`
-	while read line;do
-	
-		if [ "$(echo $line | grep SSID:)" ];then
-			#if [ "$network_1"  == "nextify" ];then
-				if [ "$(echo $line | awk '{print $2}')" != "" ];then
-					uci set wireless.default_radio0.ssid="$(echo $line | awk '{print $2}')"
-				fi
-			#fi 
-			
-			#if [ "$network_2"  == "nextify" ];then
-				if [ "$(echo $line | awk '{print $2}')" != "" ];then
-					uci set wireless.default_radio1.ssid="$(echo $line | awk '{print $2}')"
-				fi
-			#fi 				
-			
-		elif [ "$(echo $line | grep 'PASSWORD:')" ];then 
-			#if [ "$network_1"  == "nextify" ];then
-			#echo $line | awk '{print $2}'
-				if [ "$(echo $line | awk '{print $2}')" == "" ];then
-					uci delete wireless.default_radio0.encryption &> /dev/null
-					uci delete wireless.default_radio0.key &> /dev/null
-					uci delete wireless.default_radio0.rsn_preauth &> /dev/null
-				else	
-					uci set wireless.default_radio0.encryption="psk2"
-					uci set wireless.default_radio0.key="$(echo $line | awk '{print $2}')"
-					uci set wireless.default_radio0.rsn_preauth=1
-					
-				fi
-			#fi		
-			#if [ "$network_2"  == "nextify" ];then
-			#echo $line | awk '{print $2}'
-				if [ "$(echo $line | awk '{print $2}')" == "" ];then
-					uci delete wireless.default_radio1.encryption &> /dev/null
-					uci delete wireless.default_radio1.key &> /dev/null
-					uci delete wireless.default_radio1.rsn_preauth &> /dev/null
-				else	
-					uci set wireless.default_radio1.encryption="psk2"
-					uci set wireless.default_radio1.key="$(echo $line | awk '{print $2}')"
-					uci set wireless.default_radio1.rsn_preauth=1
-					
-				fi
-			#fi	
-		elif [ "$(echo $line | grep SESSIONTIMEOUT:)" ];then
-			uci set nodogsplash.@nodogsplash[0].sessiontimeout="$(echo $line | awk '{print $2}')";
-			uci set wifimedia.@nodogsplash[0].sessiontimeout="$(echo $line | awk '{print $2}')";
-		elif [ "$(echo $line | grep SERVICE:)" ];then
-			if [ "$(echo $line | awk '{print $2}')" == "enable" ];then
-				uci set nodogsplash.@nodogsplash[0].enabled='1'
-				/etc/init.d/nodogsplash enable
-			else	
-				uci set nodogsplash.@nodogsplash[0].enabled='0'
-				/etc/init.d/nodogsplash disable
-			fi	
-		fi
-	done < /tmp/config_setting
-
-	uci commit wireless && wifi
-	uci commit wifimedia
-	uci commit nodogsplash
-	/etc/init.d/nodogsplash stop
-	/etc/init.d/nodogsplash start
 }
 
 get_captive_portal_clients() {
@@ -275,18 +206,6 @@ get_captive_portal_clients() {
 	#wget --post-data="clients=${clients_ndsclt}&gateway_mac=${global_device}" http://api.nextify.vn/clients_around 2>/dev/null
     #rm /tmp/captive_portal_clients	
  }
- 
-get_config(){
-	if [ "$md5ndsconfig" != "$checkmd5file" ];then
-		echo "new config .........."
-		
-		uci set wifimedia.@nodogsplash[0].md5sum=$checkmd5file
-		uci commit wifimedia
-		setting_config
-	else
-		echo "maintain the existing settings "
-	fi
-}
 
 write_login(){
 
@@ -341,7 +260,7 @@ dhcp_extension(){
 cpn_detect(){
 	cpn_status=`uci -q get wifimedia.@nodogsplash[0].cpn`
 	if [ $cpn_status -eq 0 ];then
-		echo '*/5 * * * * /sbin/wifimedia/captive_portal.sh heartbeat'>/etc/crontabs/nds && /etc/init.d/cron restart
+		echo '* * * * * /sbin/wifimedia/captive_portal.sh heartbeat'>/etc/crontabs/nds && /etc/init.d/cron restart
 	fi
 }
 "$@"
